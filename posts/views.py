4d1d475b57d22c09c41d65a96e380119013bb583
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-05 14:31:08 +0300

    Finish basic design for the homepage

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        "caption":"new cat video",
+        "likes": "5",
+        "comments": "5",
+        "time_posted":"13hours",
+        "author": "clint-dev"
+    },
+    {
+        "caption":"new cat video",
+        "likes": "5",
+        "comments": "5",
+        "time_posted":"13hours",
+        "author": "clint-dev"
+    },
+]
+
 def home(request):
-    return render(request,"posts/home.html")
+    return render(request,"posts/home.html", {"posts":posts})
