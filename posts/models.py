211dd4b62169304746cffa163467b19892f6f5d4
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-05 16:57:49 +0300

    Add null property to picture model fields

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Picture(models.Model):
-    name = models.CharField(max_length=50)
-    image = models.ImageField()
-    date_posted = models.DateTimeField(default=timezone.now)
+    name = models.CharField(null=False, max_length=50)
+    image = models.ImageField(null=False, blank=False)
+    date_posted = models.DateTimeField(null=False, default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
