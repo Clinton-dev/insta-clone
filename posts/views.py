d6ee412afdf75a61c87115bccf6fc6d05222ca77
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-05 22:34:47 +0300

    Add user registration view

--- posts/views.py
+++ posts/views.py
@@ -1,25 +1,5 @@
-import imp
 from django.shortcuts import render
 from .models import Picture
-
-"""
-    posts = [
-    {
-        "caption":"new cat video",
-        "likes": "5",
-        "comments": "5",
-        "time_posted":"13hours",
-        "author": "clint-dev"
-    },
-    {
-        "caption":"new cat video",
-        "likes": "5",
-        "comments": "5",
-        "time_posted":"13hours",
-        "author": "clint-dev"
-    },
-    ]
-"""
 
 def home(request):
     context = {
