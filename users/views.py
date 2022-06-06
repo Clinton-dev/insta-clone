73a526b13d07a36e34117d443bef556b030391f0
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-06 08:40:31 +0300

    Add settings template to user app

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm
 
 def registration(request):
@@ -21,3 +22,7 @@ def registration(request):
         context['form'] = form
 
     return render(request, 'users/register.html', context)
+
+@login_required
+def settings(request):
+    return render(request, 'users/settings.html')
