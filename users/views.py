096de9ef8b1b6069633225ed7df39cc1015c3675
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-06 07:37:01 +0300

    Add flash message to user base html

--- users/views.py
+++ users/views.py
@@ -3,14 +3,21 @@ from django.contrib import messages
 from .forms import UserRegistrationForm
 
 def registration(request):
+    context = {
+        'title': 'signup',
+        'form': ''
+    }
+
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
+        context['form'] = form
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created successfully{username}!')
-            return redirect('home')
+            return redirect('login')
     else:
         form = UserRegistrationForm()
+        context['form'] = form
 
-    return render(request, 'users/register.html', {'form':form})
+    return render(request, 'users/register.html', context)
