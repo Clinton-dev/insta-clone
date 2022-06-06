5ef2bdf258bfd19dd2f43f4c34f232d01f3d2fc9
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-06 14:53:58 +0300

    Add update functionality to user profile

--- users/forms.py
+++ users/forms.py
@@ -31,7 +31,7 @@ class UserUpdateForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email']
 
 
 class ProfileUpdateForm(forms.ModelForm):
