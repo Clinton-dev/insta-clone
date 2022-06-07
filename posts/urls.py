d1348bf01f3445e717cb8514f99dafc99f44d6d4
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-07 09:26:49 +0300

    Add delete picture view class for posts app

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import PictureListView, PictureDetailView, PictureCreateView, PictureUpdateView
+from .views import (
+    PictureListView,
+    PictureDetailView,
+    PictureCreateView,
+    PictureUpdateView,
+    PictureDeleteView
+)
 
 urlpatterns = [
     path('', PictureListView.as_view(), name='home'),
     path('picture/<int:pk>/', PictureDetailView.as_view(), name='post-detail'),
     path('picture/new/', PictureCreateView.as_view(), name='post-new'),
-    path('picture/update/<int:pk>/', PictureUpdateView.as_view(), name='post-update')
+    path('picture/update/<int:pk>/', PictureUpdateView.as_view(), name='post-update'),
+    path('picture/delete/<int:pk>/', PictureDeleteView.as_view(), name='post-delete'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Picture
 
 def home(request):
@@ -18,6 +18,16 @@ class PictureListView(ListView):
 class PictureDetailView(DetailView):
     model= Picture
 
+class PictureDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model= Picture
+    success_url= '/'
+
+    def test_func(self):
+        picture = self.get_object()
+        if self.request.user ==  picture.author:
+            return True
+        return False
+
 class PictureCreateView(LoginRequiredMixin,CreateView):
     model= Picture
     fields = ['name', 'image']
@@ -26,10 +36,16 @@ class PictureCreateView(LoginRequiredMixin,CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class PictureUpdateView(LoginRequiredMixin, UpdateView):
+class PictureUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model= Picture
     fields = ['name', 'image']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        picture = self.get_object()
+        if self.request.user ==  picture.author:
+            return True
+        return False
