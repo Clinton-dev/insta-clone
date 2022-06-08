6d4169bd955c735d53ba25e2b69d81d605f0a4d8
Clinton wambugu <clintonwambugu@gmail.com>
2022-06-08 20:39:35 +0300

    Add test to posts app

--- insta_clone/settings.py
+++ insta_clone/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-%!p9qn$vl8&vbv(lg5o#k$8y!iv7d&zdc-!)6u*!5(4t%-$x)k'
+SECRET_KEY = 'django-insecure-%!p9qn$vl8&vbv(lg5o#k$8y!iv7d&zdc-!)6u*!5(4t%-$x)k'
 
-SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+
+from .models import Picture
+from django.contrib.auth.models import User
+
+
+class PhotoTest(TestCase):
+    def setUp(self):
+        self.user = User()
+        self.user.username = 'john doe'
+        self.user.email = 'john@doe.com'
+        self.user.save()
+
+    def test_fields(self):
+        photo = Picture()
+        photo.name = 'foo bar'
+        photo.image = 'foobar.jpg'
+        photo.author_id = self.user.id
+        photo.save()
+
+        record = Picture.objects.get(pk=1)
+        self.assertAlmostEquals(record, photo)
+
+
+
